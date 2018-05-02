fed632b225877e720a9e961e5634b9e1bbbc2758
joshuabowen0831 <joshmbowen@yahoo.com>
2018-05-01 21:22:07 -0700

    This is all my php files.

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+echo "<html>";
+echo "<title> Test page </title>";
+echo "<body>";
+echo "<h1> This is a test</h1>";
+echo "<p> This is only a test</p>";
+echo "</body>";
+echo "</html>";
+?>
