c0d9a0149a1456719fb8dfc1ece8816e9aa478d7
joshuabowen0831 <joshmbowen@yahoo.com>
2018-05-02 22:33:19 -0700

    This is a sample array file created by andrew that I am to change.

---
+++ Arrays2.php
@@ -0,0 +1,21 @@
+More about arrays
+
+Example 1:
+<?php
+// Variables
+
+$a = array(1,2,3); 
+Echo $a[1];
+// This prints 2.
+?>
+
+Example 2: Changing the value in an Array
+
+<?php
+$a = array(1,2,3);
+$a = array(0 => 0);
+Echo $a[0]; //this prints 0.
+$a[1] = 4;
+Echo $a[1];// This now prints 4.
+?>
+
