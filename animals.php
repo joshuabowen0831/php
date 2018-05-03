8f92e72e84f1af8c33ba611973c57614d9b6c97e
joshuabowen0831 <joshmbowen@yahoo.com>
2018-05-03 12:59:14 -0700

    updating some files

---
+++ animals.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title> Pet Form Information </title>
+</head>
+<body>
+<h1> First form with all text fields </h1>
+<p>
+<?php
+echo "Your pet's name is ".$_POST["Name"].".";
+echo " It is ".$_POST["age"]." years old and is ".$_POST["weight"]." pounds.";
+echo " Your pet is a ".$_POST["species"]." and is a ".$_POST["breed"].".";
+echo "<h2> This is the second form with radio buttons </h2>";
+echo "Your pet's name is ".$_POST["pet_name"];
