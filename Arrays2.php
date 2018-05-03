b27b2939fbfe1345041adfd2103263d01718fd89
joshuabowen0831 <joshmbowen@yahoo.com>
2018-05-03 02:59:31 -0700

    I messed around with this file to create some arrays.:

--- Arrays2.php
+++ Arrays2.php
@@ -19,3 +19,43 @@ $a[1] = 4;
 Echo $a[1];// This now prints 4.
 ?>
 
+Example 3: my own array of numbers and what I think their values will be when echoed
+
+<?php
+$numbers = array(2,4,6);
+echo $numbers[0];
+//this should print 2
+echo $numbers[1];
+//this should print 4
+echo $numbers[2];
+//this should print 6
+?>
+example 5 changing value of numbers in an array and printing the results
+
+<?php
+$numbers = array(1,3,5);
+$numbers[1] = 7;
+$numbers[3] = 14;
+$numbers[5] = 21;
+
+echo $numbers[1];
+//this should print 7
+echo $numbers[3];
+//this should print 14
+echo $numbers[5];
+//this should print 21
+
+?>
+example 6 creating a string array and changing it's values
+<?php
+$music = array("Country","Rock","Pop");
+echo $music[0];
+echo <<<END
+<br>
+$music[1]
+<br>
+$music[2]
+END
+;
+?>
+
