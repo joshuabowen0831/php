4669eb39d87fcd2ec6c64136a71f44d4d1fa72b0
joshuabowen0831 <joshmbowen@yahoo.com>
2018-05-04 13:15:00 -0700

    This is a file to make a deck of cards

---
+++ cards.php
@@ -0,0 +1,7 @@
+<?php
+$cards = array( array(2,3,4,5,6,7,8,9,10,"jack","queen","king","ace"),array("clubs","dimonds","spades","hearts"));
+
+//This will attempt to ref a card combo of my choice
+echo $cards[0][1]." of ".$cards[1][2];
+?>
+
