77c923d4850650c84ee1fcf42d41c7debddbf777
joshuabowen0831 <joshmbowen@yahoo.com>
2018-05-04 13:25:16 -0700

    an example of a multi array.

--- multi_array.php
+++ multi_array.php
@@ -1,8 +1,9 @@
 <?php
 //This is to play around with a multi array
-$people = array( array("Josh","Bowen",35,"Male"),array("Andrew","Meyer",36,"Male),array("Kim","Castro",35,"Female"));
+$people = array( array("Josh","Bowen",35,"Male"),array("Andrew","Meyer",36,"Male"),array("Kim","Castro",35,"Female"));
 
 //next going to echo different references
-echo "$people[0][0";
+echo $people[0][1].", ".$people[0][2];
+
 ?>
 
