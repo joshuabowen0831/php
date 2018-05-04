16539ef88b3c93b85654005ec3cf146d62ce4e1c
joshuabowen0831 <joshmbowen@yahoo.com>
2018-05-04 13:49:58 -0700

    made some edits to cards.php

--- cards.php
+++ cards.php
@@ -1,7 +1,9 @@
 <?php
-$cards = array( array(2,3,4,5,6,7,8,9,10,"jack","queen","king","ace"),array("clubs","dimonds","spades","hearts"));
+//the following line creates an array which makes a deck of playing cards.
 
-//This will attempt to ref a card combo of my choice
+$cards = array( array(2,3,4,5,6,7,8,9,10,"jack","queen","king","ace"),array("clubs","diamonds","spades","hearts"));
+
+//the next line prints a combination of card rank and suit from the array above
 echo $cards[0][1]." of ".$cards[1][2];
 ?>
 
