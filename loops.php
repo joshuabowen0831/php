6c80ff8b633d68341e65987e72553d2b632a9234
joshuabowen0831 <joshmbowen@yahoo.com>
2018-05-10 22:12:10 -0700

    updated loops file

--- loops.php
+++ loops.php
@@ -22,6 +22,27 @@ echo $value;
 ?>
 </tr>
 </table>
+
+<h2> The following cards table will be made with a do while loop and a foreach loop </h2>
+<table style="width:100%">
+<tr> <th> Card Rank</th> <th> Card Suit </th> </tr>
+<?php
+$i = 1;
+//The following do while loop fills in the table with the values 2 throught 10 and also the suit hearts
+
+do {
+$i++;
+   echo "<tr><td>".$i."</td><td>Hearts</td></tr>";
+} while ($i < 10);
+//the following foreach loop completes the table buy printing the values jack through ace and the suit hearts
+$hearts = array("<tr><td>Jack</td><td>Hearts</td></tr>","<tr><td>Queen</td><td>Hhearts</td></tr>","<tr><td>King</td><td>Hearts</td></tr>","<tr><td>Ace</td><td>Hearts</td></tr>");
+foreach($hearts as $value) {
+echo $value;
+}
+?>
+</table>
+<h3> The next rank and suit card table will be constructed by a for loop </h3>
+
 </body>
 </html>
 
